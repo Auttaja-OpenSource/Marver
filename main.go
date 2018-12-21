d9c5f95c646e2081f591f8034bcbcb1fee05cd2b
Jacob Wiltse <jwiltse@control-tec.com>
2018-12-21 14:53:41 -0500

    Correct the copyright header

--- main.go
+++ main.go
@@ -1,6 +1,6 @@
 /*
 Marver
-Copyright (C) 2018  Auttaja
+Copyright (C) 2018  Jacob Wiltse
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
